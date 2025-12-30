25d76f2e0f14063aab558258357a7299c69a97f7
Aderson <iamuchoa@gmail.com>
2025-12-29 22:24:21 -0300

    close modal nos modais

--- src/components/ImovelModal.vue
+++ src/components/ImovelModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isOpen" class="modal-overlay" @click="closeModal">
+  <div v-if="isOpen" class="modal-overlay">
     <div class="modal-content" @click.stop>
       <div class="modal-header">
         <h2>{{ isEditMode ? 'Editar Imóvel' : 'Novo Imóvel' }}</h2>
